7092720a2a876d04a5fe596e8a2feb3a0036606d
dexter21767 <ahmidiyasser@egmail.com>
2022-08-27 01:40:37 +0100

    add multiresolutions

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const addonInterface = require("./addon")
 serveHTTP(addonInterface, { port: process.env.PORT || 63355 })
 
 // when you've deployed your addon, un-comment this line
-//publishToCentral("http://2ecbbd610840-hi-movies.baby-beamup.club/manifest.json")
+publishToCentral("http://2ecbbd610840-hi-movies.baby-beamup.club/manifest.json")
 // for more information on deploying, see: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/deploying/README.md
